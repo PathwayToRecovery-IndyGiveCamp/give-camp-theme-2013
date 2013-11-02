b87514f26712d0e474fdd968d38d134b1ce380d8
Alex Vernacchia <avernacchia@ubuntu.(none)>
2013-11-02 07:37:04 -0700

    modified files

---
+++ content-home.php
@@ -0,0 +1,10 @@
+<div>home page template content</div>
+<div><?php the_content(); ?></div>
+<div><?php the_block('Headline Top'); ?></div>
+
+
+<div><?php the_block('Image or Text Link Left'); ?></div>
+<div><?php the_block('Image or Text Link Middle'); ?></div>
+<div><?php the_block('Image or Text Link Right'); ?></div>
+
+<div><?php the_block('Headline Bottom'); ?></div>
--- page-home.php
+++ page-home.php
@@ -1,11 +1,14 @@
 <?php
-// Default page template
+/*
+Template Name: Home Page Template
+*/
 
 get_header();
 
   while ( have_posts() ) { 
     the_post();
-    get_template_part( 'content', 'page' );
+    get_template_part( 'content', 'home' );
+
    } // end of the loop
 
 get_footer();
