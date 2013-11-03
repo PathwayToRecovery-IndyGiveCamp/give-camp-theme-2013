83aebe3da9c90f01337906cb0986488b0b1f0260
Alex Vernacchia <avernacchia@exacttarget.com>
2013-11-03 06:08:23 -0800

    updating 404 page

--- 404.php
+++ 404.php
@@ -4,12 +4,14 @@ get_header();
 
 ?>
 
-  <h3 style="color:#666;">Opps...</h3>
-  <p><strong>Looks like this page <em style="color:#888">(<?php echo $_SERVER['REQUEST_URI']; ?>)</em> doesn't exist. Look what you found though.</strong></p>
-  <div class="video-container">  
-    <iframe width="620" height="465" src="http://www.youtube.com/embed/oHg5SJYRHA0?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
-  </div>
-  <p>I had to, sorry!</p>
+  <section>
+  	<div class="row">
+  		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+  			<h1>404</h1>
+  			<p>It looks like you are in the wrong place. Go to the <a href="/">home page</a>.</p>
+  		</div>
+  	</div>
+  </section>
         
 <?php
 get_footer();
