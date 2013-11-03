ef052fac76019768f5089ca7babc80a78e80387a
Alex Vernacchia <avernacchia@exacttarget.com>
2013-11-03 10:46:58 -0500

    updating full page

--- content-page-full.php
+++ content-page-full.php
@@ -1,17 +1,12 @@
-<?php
-
-get_header();
-
-?>
-
-  <section>
-  	<div class="row">
-  		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-  			<h1><?php the_title(); ?></h1>
-  			<?php the_content(); ?>
-  		</div>
-  	</div>
+<section class="page-content-bottom-container">
+  <section class="home-content-bottom page-content-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <h1><?php the_title(); ?></h1>
+          <?php the_content(); ?>
+        </div>
+      </div>
+    </div>
   </section>
-        
-<?php
-get_footer();
+</section>
