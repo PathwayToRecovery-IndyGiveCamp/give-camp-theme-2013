91d212f02afe369f5afcdce4bedfc1d9c806c393
Alex Vernacchia <avernacchia@exacttarget.com>
2013-11-03 07:13:06 -0800

    updating more styles

--- footer.php
+++ footer.php
@@ -1,98 +1,100 @@
-<footer class="site-footer">
-    <div class="container">
-      <div class="row footer-content">
-        <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
-          <h4>Pages</h4>
-          <?php
-          $menu_name = 'primary';
+  </div><!-- end of .push -->
+  <footer class="site-footer">
+      <div class="container">
+        <div class="row footer-content">
+          <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+            <h4>Pages</h4>
+            <?php
+            $menu_name = 'primary';
 
-          if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
-            $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+            if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
+              $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
 
-            $menu_items = wp_get_nav_menu_items($menu->term_id);
+              $menu_items = wp_get_nav_menu_items($menu->term_id);
 
-            if( count( $menu_items ) > 0 ) {
+              if( count( $menu_items ) > 0 ) {
 
-              $menu_list = '<ul>';
+                $menu_list = '<ul>';
 
-              foreach ( (array) $menu_items as $key => $menu_item ) {
-                
-                if( $menu_item->post_parent == 0 ) {
-                  $title = $menu_item->title;
-                  $url = $menu_item->url;
-                  $menu_list .= '<li><a href="' . $url . '">' . $title . '</a></li>';
+                foreach ( (array) $menu_items as $key => $menu_item ) {
+                  
+                  if( $menu_item->post_parent == 0 ) {
+                    $title = $menu_item->title;
+                    $url = $menu_item->url;
+                    $menu_list .= '<li><a href="' . $url . '">' . $title . '</a></li>';
+                  }
                 }
+                $menu_list .= '</ul>';
               }
-              $menu_list .= '</ul>';
+            } else {
+              $menu_list = '';
             }
-          } else {
-            $menu_list = '';
-          }
-          echo $menu_list;
-          ?>
-        </div>
-        <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
-          <h4>Resources</h4>
-          <?php
-          $menu_name = 'resources';
+            echo $menu_list;
+            ?>
+          </div>
+          <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+            <h4>Resources</h4>
+            <?php
+            $menu_name = 'resources';
 
-          if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
-            $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+            if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
+              $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
 
-            $menu_items = wp_get_nav_menu_items($menu->term_id);
+              $menu_items = wp_get_nav_menu_items($menu->term_id);
 
-            if( count( $menu_items ) > 0 ) {
+              if( count( $menu_items ) > 0 ) {
 
-              $menu_list = '<ul>';
+                $menu_list = '<ul>';
 
-              foreach ( (array) $menu_items as $key => $menu_item ) {
-                
-                if( $menu_item->post_parent == 0 ) {
-                  $title = $menu_item->title;
-                  $url = $menu_item->url;
-                  $menu_list .= '<li><a href="' . $url . '">' . $title . '</a></li>';
+                foreach ( (array) $menu_items as $key => $menu_item ) {
+                  
+                  if( $menu_item->post_parent == 0 ) {
+                    $title = $menu_item->title;
+                    $url = $menu_item->url;
+                    $menu_list .= '<li><a href="' . $url . '">' . $title . '</a></li>';
+                  }
                 }
+                $menu_list .= '</ul>';
               }
-              $menu_list .= '</ul>';
+            } else {
+              $menu_list = '';
             }
-          } else {
-            $menu_list = '';
-          }
-          echo $menu_list;
-          ?>
-        </div>
-        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 hidden-xs">
-          <h4>Contact</h4>
-          <address>
-            <strong>Pathway to Recovery</strong><br/>
-            2135 N. Alabama St<br/>
-            Indianapolis, IN 46202<br/>
-            <abbr title="Phone">P:</abbr> (317) 926-8557<br />
-            <abbr title="Fax">F:</abbr> (317) 926-2250
-          </address>
-        </div>
-      </div>
-      <div class="row visible-xs">
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-          <h4>Contact</h4>
-          <address>
-            <strong>Pathway to Recovery</strong><br/>
-            2135 N. Alabama St<br/>
-            Indianapolis, IN 46202<br/>
-            <abbr title="Phone">P:</abbr> (317) 926-8557<br />
-            <abbr title="Fax">F:</abbr> (317) 926-2250
-          </address>
-        </div>
-      </div>
-      <section class="copyright">
-        <div class="row">
-          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            Copyright 2013 Pathway to Recovery, Inc. / CFC 56320 / SECC 686300
+            echo $menu_list;
+            ?>
+          </div>
+          <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 hidden-xs">
+            <h4>Contact</h4>
+            <address>
+              <strong>Pathway to Recovery</strong><br/>
+              2135 N. Alabama St<br/>
+              Indianapolis, IN 46202<br/>
+              <abbr title="Phone">P:</abbr> (317) 926-8557<br />
+              <abbr title="Fax">F:</abbr> (317) 926-2250
+            </address>
           </div>
         </div>
-      </section>
-    </div>
-  </footer>
+        <div class="row visible-xs">
+          <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <h4>Contact</h4>
+            <address>
+              <strong>Pathway to Recovery</strong><br/>
+              2135 N. Alabama St<br/>
+              Indianapolis, IN 46202<br/>
+              <abbr title="Phone">P:</abbr> (317) 926-8557<br />
+              <abbr title="Fax">F:</abbr> (317) 926-2250
+            </address>
+          </div>
+        </div>
+        <section class="copyright">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              Copyright 2013 Pathway to Recovery, Inc. / CFC 56320 / SECC 686300
+            </div>
+          </div>
+        </section>
+      </div>
+    </footer>
+  </div><!-- end of wrapper -->
   <?php wp_footer(); ?>
   <script type="text/javascript">
     var _gaq = _gaq || [];
--- header.php
+++ header.php
@@ -26,49 +26,51 @@
     <?php wp_head(); ?>
   </head>
   <body>
-    <div class="container facebook-link">
-    <a href="https://www.facebook.com/pages/Pathway-to-Recovery/160535554009736"></a>
-  </div>
+    <div class="wrapper">
+      <div class="container facebook-link">
+        <a href="https://www.facebook.com/pages/Pathway-to-Recovery/160535554009736"></a>
+      </div>
 
-  <section class="logos">
-    <div class="container clearfix">
-      <a href="<?php echo bloginfo('wpurl'); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php bloginfo('name'); ?>" /></a>
-      <h1>Sober Living for Homeless Hoosiers</h1>
-    </div>  
-  </section>
+      <section class="logos">
+        <div class="container clearfix">
+          <a href="<?php echo bloginfo('wpurl'); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php bloginfo('name'); ?>" /></a>
+          <h1>Sober Living for Homeless Hoosiers</h1>
+        </div>  
+      </section>
 
-  <section class="navigation">
-    <div class="container">
-      <nav class="navbar navbar-default navigation-bottom" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand visible-xs" href="#">Pathway to Recovery</a>
+      <section class="navigation">
+        <div class="container">
+          <nav class="navbar navbar-default navigation-bottom" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand visible-xs" href="#">Pathway to Recovery</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                  <?php 
+                  wp_nav_menu( 
+                    array(
+                      'theme_location' => 'primary',
+                      'sort_column'    => 'menu_order', 
+                      'container'      => 'ul',
+                      'container_class' => '',
+                      'menu_class'     => '',
+                      'items_wrap'     => '%3$s',
+                      'depth'          => 2
+                    )
+                  );
+                  ?>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+          </nav>
         </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <?php 
-              wp_nav_menu( 
-                array(
-                  'theme_location' => 'primary',
-                  'sort_column'    => 'menu_order', 
-                  'container'      => 'ul',
-                  'container_class' => '',
-                  'menu_class'     => '',
-                  'items_wrap'     => '%3$s',
-                  'depth'          => 2
-                )
-              );
-              ?>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-      </nav>
-    </div>
-  </section>
+      </section>
+      <div class="push">
